6c8c7d8afe664e0fcca3af850a870d6bcd1a8df6
tojinn <jin.10319922@gmail.com>
2022-02-26 12:07:36 +0000

    [fix]

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def send_mail(contact)
     @contact = contact
-    mail to: TOMAIL, subject: 'お問い合わせ内容'+ @contact.subject_i18n
+    mail to: ENV["TOMAIL"], subject: 'お問い合わせ内容'+ @contact.subject_i18n
   end
 end
